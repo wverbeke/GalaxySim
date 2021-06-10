1d2b1385bc5bb783d1dd40a2cae82aa0401bb764
wverbeke <willem.verbeke@cern.ch>
2021-06-10 22:06:01 +0200

    Implement first draft of Body class
    Body represents a mass in the N-body simulation, which can be a star,
    planet, asteroid and so forth. Each body has a position, velocity and
    mass, and functions to update the position and velocity.

---
+++ body/test/body_test.cc
@@ -0,0 +1,20 @@
+#include "../include/Body.h"
+#include "../../vector/include/Vector2D.h"
+#include "../../vector/include/Vector3D.h"
+
+int main(){
+    Body<Vector2D<float>> body_2D_float;
+    Body<Vector2D<double>> body_2D_double;
+    Body<Vector3D<float>> body_3D_float;
+    Body<Vector3D<double>> body_3D_double;
+
+    body_3D_float.position();
+    body_3D_float.velocity();
+    body_3D_float.mass();
+
+    Vector3D<float> position_update{1., 2., 3.};
+    body_3D_float.updatePosition(position_update);
+
+    Vector3D<float> velocity_update{3., 2., 1.};
+    body_3D_float.updateVelocity(velocity_update);
+}
