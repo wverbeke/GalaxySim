644dae427e4d8e32a0f7e3e8d1ee718b9ffaa4f3
wverbeke <willem.verbeke@cern.ch>
2022-12-28 16:32:56 +0100

    Simpler names for numeric type functions

--- io/include/numeric_types.h
+++ io/include/numeric_types.h
@@ -4,32 +4,32 @@
 #include <stdexcept>
 #include "hdf5.h"
 
-template<typename T> hid_t get_h5_memory_type(){
+template<typename T> hid_t h5_memory_type(){
     throw std::domain_error("Invalid type for I/O to HDF5 file.");
 }
 
 
-template<> hid_t get_h5_memory_type<float>(){
+template<> hid_t h5_memory_type<float>(){
     return H5T_NATIVE_FLOAT;
 }
 
 
-template<> hid_t get_h5_memory_type<double>(){
+template<> hid_t h5_memory_type<double>(){
     return H5T_NATIVE_DOUBLE;
 }
 
 
-template<typename T> hid_t get_h5_file_type(){
+template<typename T> hid_t h5_file_type(){
     throw std::domain_error("Invalid type for I/O to HDF5 file.");
 }
 
 
-template<> hid_t get_h5_file_type<float>(){
+template<> hid_t h5_file_type<float>(){
     return H5T_IEEE_F32BE;
 }
 
 
-template<> hid_t get_h5_file_type<double>(){
+template<> hid_t h5_file_type<double>(){
     return H5T_IEEE_F64BE;
 }
 #endif
--- io/test/numeric_types_test.cc
+++ io/test/numeric_types_test.cc
@@ -1,8 +1,9 @@
 #include "../include/numeric_types.h"
 
 int main(){
-    get_h5_memory_type<double>();
-    get_h5_memory_type<float>();
-    get_h5_file_type<float>();
-    get_h5_file_type<float>();
+    h5_memory_type<double>();
+    h5_memory_type<float>();
+    h5_file_type<float>();
+    h5_file_type<float>();
+    return 0;
 }
